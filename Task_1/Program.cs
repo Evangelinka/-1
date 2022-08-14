9c15717410e824fac6306243d89189927c740263
«Kristina» <kristinabol1996@gmail.com>
2022-08-14 21:07:14 +0800

    1 задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа, которая на вход принимает два числа и выдает, какое число большее.
+
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(number1);
+}
+else
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(number2);
+}
